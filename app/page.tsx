cec19b0904c6d04ec7a95af5c94d91bb17218649
prabuddhaxdev <prabuddha1749@gmail.com>
2025-09-11 20:09:58 +0530

    Add Spinners

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import EventList from "@/components/EventList";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="">
+      <EventList />
     </div>
   );
 }
