05ae4017d1b755ce818504c8bd182dce1eccb21a
prabuddhaxdev <prabuddha1749@gmail.com>
2025-09-18 02:40:38 +0530

    Fixed errors with clerk middleware auth

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+// Define all protected routes
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/tickets(.*)", // includes /tickets/purchase-success
+]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Run Clerk middleware on all routes except static files, Next.js internals, and favicon
+    "/((?!_next|.*\\..*|favicon.ico).*)",
   ],
-}
+};
