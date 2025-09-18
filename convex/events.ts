e26aa04d6e8285d58a98d9166006387c6cedbec4
prabuddhaxdev <prabuddha1749@gmail.com>
2025-09-18 20:26:30 +0530

    Add Search Functionality

--- convex/events.ts
+++ convex/events.ts
@@ -1,9 +1,9 @@
 import { query, mutation } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 import { DURATIONS, WAITING_LIST_STATUS, TICKET_STATUS } from "./constants";
-import { internal } from "./_generated/api";
+import { components, internal } from "./_generated/api";
 import { processQueue } from "./waitingList";
-
+import { MINUTE, RateLimiter } from "@convex-dev/rate-limiter";
 
 export type Metrics = {
   soldTickets: number;
@@ -12,7 +12,14 @@ export type Metrics = {
   revenue: number;
 };
 
-
+// Initialize rate limiter
+const rateLimiter = new RateLimiter(components.rateLimiter, {
+  queueJoin: {
+    kind: "fixed window",
+    rate: 3, // 3 joins allowed
+    period: 30 * MINUTE, // in 30 minutes
+  },
+});
 
 export const get = query({
   args: {},
@@ -105,15 +112,15 @@ export const joinWaitingList = mutation({
   // Function takes an event ID and user ID as arguments
   args: { eventId: v.id("events"), userId: v.string() },
   handler: async (ctx, { eventId, userId }) => {
-    // // Rate limit check
-    // const status = await rateLimiter.limit(ctx, "queueJoin", { key: userId });
-    // if (!status.ok) {
-    //   throw new ConvexError(
-    //     `You've joined the waiting list too many times. Please wait ${Math.ceil(
-    //       status.retryAfter / (60 * 1000)
-    //     )} minutes before trying again.`
-    //   );
-    // }
+    // Rate limit check
+    const status = await rateLimiter.limit(ctx, "queueJoin", { key: userId });
+    if (!status.ok) {
+      throw new ConvexError(
+        `You've joined the waiting list too many times. Please wait ${Math.ceil(
+          status.retryAfter / (60 * 1000)
+        )} minutes before trying again.`
+      );
+    }
 
     // First check if user already has an active entry in waiting list for this event
     // Active means any status except EXPIRED
